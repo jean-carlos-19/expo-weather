b2068571d1ad61024f53d7b7ef54c90a8d3dbf38
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-24 21:18:10 -0500

    Modificado el archivo Home.tsx: añadiendo la vista Home

---
+++ src/views/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SafeAreaView,Text } from 'react-native'
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>pantalla home</Text>
+    </SafeAreaView>
+  )
+}
+
+export  {Home}
