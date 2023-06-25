66568fec4669b526a31cfbb7126632aad7613685
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-24 21:19:38 -0500

    Modificado el archivo babel.config.js y tsconfig.json: añadiendo rutas de alias views, models controllers, services

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins:[
+      [
+        'module-resolver',
+        {
+          'root':["."],
+          'alias':{
+            "controllers":"./src/controllers",
+            "models":"./src/models",
+            "services":"./src/services",
+            "views":"./src/views",
+          }
+        }
+      ],
+    ]
   };
 };
