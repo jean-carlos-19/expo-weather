3de14fa8e0dd42f6bdd9a9766e699db87254a7a4
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-25 20:37:16 -0500

    Modificado el archivo App.tsx: añadiendo la vista Home

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { LogBox } from 'react-native';
 import { Home } from 'views'
 
 const Stack = createNativeStackNavigator();
+
+LogBox.ignoreLogs([
+  'Non-serializable values were found in the navigation state',
+]);
 
 export default function App() {
   return (
@@ -15,12 +18,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
