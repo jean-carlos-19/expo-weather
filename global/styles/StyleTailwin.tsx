dba8c6f468f45de0736dc427cd16d1acbf426087
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-25 20:35:29 -0500

    Modificado el archivo StyleTailwin.tsx: añadiendo los componentes de estilos de tailwin

---
+++ global/styles/StyleTailwin.tsx
@@ -0,0 +1,10 @@
+import { styled } from 'nativewind';
+import { ImageBackground, View } from 'react-native'
+
+const StyledView = styled(View)
+const StyledImage = styled(ImageBackground)
+
+export {
+    StyledImage,
+    StyledView
+}
