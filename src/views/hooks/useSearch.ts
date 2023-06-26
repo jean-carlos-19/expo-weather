4fd9dadc09e16f50ad04adda2bee493a24f0eae1
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-26 11:27:11 -0500

    modificado el archivo useSearch.ts: añdiendo la funcion hand_location

--- src/views/hooks/useSearch.ts
+++ src/views/hooks/useSearch.ts
@@ -1,5 +1,18 @@
+import { useState } from "react"
 
 const useSearch = ()=>{
     
+    const [search,set_search]= useState();
+    const [show_search,set_show_search]= useState(false);
+
+    const toogle_search =()=>{
+        set_show_search(!show_search)
+    }
+
+    const hand_location =(location:string)=>{
+        console.log("Location: "+location)
+    }
+
+    return {show_search,search,toogle_search,hand_location}
 }
 export {useSearch}
