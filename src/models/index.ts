2a18287fd684100d19065f136d58df8f881b117a
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-27 16:48:09 -0500

    Creado los archivos index.ts search.dto.ts weather.model.ts junto con su logica

---
+++ src/models/index.ts
@@ -0,0 +1,2 @@
+export type {Weather_Model} from "./weather.model";
+export type {Search_Dto,Forecast_Dto} from "./search.dto";
---
+++ src/models/weather.model.ts
@@ -0,0 +1,4 @@
+interface Weather_Model{
+
+}
+export type {Weather_Model}
