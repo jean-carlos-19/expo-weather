627b7cc225fc706c9a9149822ca14328341e5605
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-27 16:33:27 -0500

    Actualizando los archivos de configuracion del proyecto para los path de  alias y la constuccion del apk

--- babel.config.js
+++ babel.config.js
@@ -13,13 +13,21 @@ module.exports = function (api) {
             "@/services": "./src/services",
             "@/views": "./src/views",
             "@/global/theme": "./global/theme",
-            "@/global/atomic/element": "./global/atomic/component",
+            "@/global/img": "./global/img/img.ts",
+            "@/global/atomic/element": "./global/atomic/element",
             "@/global/atomic/component": "./global/atomic/component",
-            "@/global/atomic/design": "./global/atomic/component",
+            "@/global/atomic/design": "./global/atomic/design",
           },
         },
       ],
       ["nativewind/babel"],
+      [
+        "module:react-native-dotenv",
+        {
+         'moduleName':'@env',
+         'path':'.env',
+        }
+      ]
     ],
   };
 };
