0474600c92dd807e5a5a4e5b07704c17ab9f31ac
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-27 16:37:00 -0500

    Actualizando lsoa archivos de configuracion

---
+++ env.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+    export const URL_LOCAL: string;
+    export const REACT_API_WEATHER:string;
+    export const REACT_API_SEARCH_WEATHER:string;
+    export const REACT_API_FORECAST_WEATHER:string;
+}
